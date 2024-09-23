fb9fb655286ee57a12653b4e298d6daf347290a9
ellen.jarvenpaa <ellen.jarvenpaa@gmail.com>
2024-09-23 16:21:21 +0300

    mfe tehtävä

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css';
+import Profile from './views/profile/Profile';
+import Upload from './views/upload/Upload';
+import ProtectedRoute from './components/ProtectedRoute';
+import Layout from './views/Layout';
+import Home from './views/home/Home';
+import { UserProvider } from 'mediastore/UserContext';
+import { MediaProvider } from 'mediastore/MediaContext';
+
+function App() {
+  return (
+    <Router>
+      <UserProvider>
+        <MediaProvider>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route
+                path="/upload"
+                element={
+                  <ProtectedRoute>
+                    <Upload />
+                  </ProtectedRoute>
+                }
+              />
+            </Route>
+          </Routes>
+        </MediaProvider>
+      </UserProvider>
+    </Router>
+  );
+}
+
+export default App;
